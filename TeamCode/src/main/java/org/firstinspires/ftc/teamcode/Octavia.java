138dd5f40a1ec31e2147dd9a2690ee8300fee46d
tsmckinney <tsmckinney10@gmail.com>
2026-03-09 19:59:56 -0400

    Removed some unnecessary code That was all mainly just for documentation purposes. We don't really need to have it laying around. I also added the ITO motors to Octavia's program board.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Octavia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Octavia.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.boardmeeting.DriveBase;
@@ -12,25 +13,16 @@ import org.firstinspires.ftc.teamcode.boardmeeting.ProgramBoard;
 public class Octavia extends ProgramBoard
 {
     public Config cfg;
+    public DcMotor ito_transfer;
+    public DcMotor ito_outtake;
+
     public void init(HardwareMap map){
         super.init(map); // Run this first to avoid null-pointer exceptions.
 
         dbase = new DriveBase(cfg, map);
 
-        /* Note from Toby: I had originally programmed this to test our Board Meeting library,
-         * but since Octavia mostly uses motors, we don't actually need any of this code.
-         * What we *do* need to do is initialize the IMU.
-         */
-        //sensors.addItem(map, "touch sensor");
-        //sensors.getItem("touch sensor").setMode(DigitalChannel.Mode.INPUT);
-
-        //servos.addItem(map, "servo");
-
-        //analogSensors.addItem(map, "analog sensor");
-
-        //colorSensors.addItem(map, "color sensor");
-
-        //distanceSensors.addItem(map, "distance sensor");
+        ito_transfer = motors.addItem(map, cfg.ITO_TRANSFER);
+        ito_outtake = motors.addItem(map, cfg.ITO_OUTTAKE);
 
         RevHubOrientationOnRobot orientation = new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
